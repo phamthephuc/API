9e418237e1ba5d757f676c22359792cf3d4710da
CaoAnhDao1804 <caoanhdao18041996@gmail.com>
2018-11-04 15:14:14 +0700

    Get comment of location

--- src/main/java/com/repository/CommentRepository.java
+++ src/main/java/com/repository/CommentRepository.java
@@ -4,6 +4,10 @@ import com.entity.Comment;
 import com.entity.PlaceType;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+
+    public List<Comment> findTop10ByIdLocation(Long idLocation);
 
 }
--- src/main/java/com/service/ReviewService.java
+++ src/main/java/com/service/ReviewService.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class ReviewService {
