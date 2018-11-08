aa6ce5103b8cb5efb51ebea014beb011de18ba9b
CaoAnhDao1804 <caoanhdao18041996@gmail.com>
2018-11-08 10:27:14 +0700

    Fix

--- src/main/java/com/dto/UserRegisterDTO.java
+++ src/main/java/com/dto/UserRegisterDTO.java
@@ -11,16 +11,21 @@ import javax.validation.constraints.Size;
 
 public class UserRegisterDTO {
 
-    @NotBlank(message = "Username must be asleat 4 charater and maximum 200")
-    @Email
-    @Size(min=4, max=200)
+    @NotBlank(message = "Username couldn't be blank!")
+    @Size(min=4, max=200, message = "Username have lenght between 4 and 200!")
     private String username;
 
-    @NotBlank
-    @Size(min=6, max=30)
+    @NotBlank(message = "Password couldn't be blank!")
+    @Size(min=6, max=30, message = "Username have lenght between 4 and 200!")
     private String password;
 
+    @NotBlank(message = "Address couldn't be blank!")
     private String address;
+
+    @NotBlank(message = "Address couldn't be blank!")
+    @Size(min = 10, max = 11)
     private String phone;
+
+    @NotBlank
     private Long gender;
 }
