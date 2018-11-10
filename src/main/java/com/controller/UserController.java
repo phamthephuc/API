a9cac6ecf495174ed71ce4f633cf2b74107445bb
CaoAnhDao1804 <caoanhdao18041996@gmail.com>
2018-11-10 10:58:53 +0700

    fix user profile

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,8 +1,19 @@
 package com.controller;
 
+import com.dto.APIResponseDTO;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class UserController {
+
+    @GetMapping(value = "/api/user-prfile")
+    public APIResponseDTO getUserProfile(){
+
+        return new APIResponseDTO();
+    }
+
+
+
 }
