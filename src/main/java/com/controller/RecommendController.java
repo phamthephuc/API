bb07de8deeecd915d94d124acf833bad1b9a5dbc
CaoAnhDao1804 <caoanhdao18041996@gmail.com>
2018-11-14 11:09:08 +0700

    Fix recommend

--- src/main/java/com/controller/RecommendController.java
+++ src/main/java/com/controller/RecommendController.java
@@ -1,7 +1,9 @@
 package com.controller;
 
 import com.dto.APIResponseDTO;
+import com.entity.Users;
 import com.service.RecommendService;
+import com.service.UsersService;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @CrossOrigin(origins = "*", maxAge = 1800)
 public class RecommendController {
@@ -17,13 +21,21 @@ public class RecommendController {
     @Autowired
     RecommendService recommendService;
 
-    @GetMapping(value = "/locations/recommends/{idUser}")
+    @Autowired
+    UsersService usersService;
+    @GetMapping(value = "/locations/recommends")
     @ApiResponses(value = {//
             @ApiResponse(code = 400, message = "Something went wrong"), //
             @ApiResponse(code = 403, message = "Access denied"), //
             @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
-    public APIResponseDTO findRecommend(@PathVariable Long idUser){
-        return  new APIResponseDTO(200,"Success!",recommendService.getListLocationProfileDTORecommend(idUser));
+    public APIResponseDTO findRecommend(HttpServletRequest request){
+        Users userCurrent = usersService.findUserFromToken(request);
+        if (userCurrent != null){
+            return  new APIResponseDTO(200,"Success!",recommendService.getListLocationProfileDTORecommend(userCurrent.getId()));
+        } else {
+            return  new APIResponseDTO(500,"Token wrong!",null);
+
+        }
     }
 
 
